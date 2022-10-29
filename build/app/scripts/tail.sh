29256664d161b978daa22976a0bf2a3ae0a11292
toddwint <todd.wintermute@gmail.com>
2022-10-28 22:58:12 -0500

    fixed utempter error, made tmux better

--- build/app/scripts/tail.sh
+++ build/app/scripts/tail.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 echo '
   Welcome to the '"$APPNAME"' docker image.
-
-  Captures will be saved to the `captures` volume specified in the 
-  docker run command.
 '
 sleep 1s
-tail -n 500 -F /opt/"$APPNAME"/logs/"$APPNAME".log
+tail -n 500 --pid=$$ -F /opt/"$APPNAME"/logs/"$APPNAME".log
--- build/app/scripts/tmux.sh
+++ build/app/scripts/tmux.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-tmux new-session -s "$APPNAME" -d
-tmux send-keys -t 1 "tail -n 500 -F /opt/$APPNAME/logs/$APPNAME.log" Enter
-#tmux selectp -t 1 -d #disable user input in pane
-#tmux selectp -t 1 -e #enable user input in pane
 echo '
   Welcome to the '"$APPNAME"' docker image.
-
-  Captures will be saved to the `captures` volume specified in the 
-  docker run command.
 
   This application uses tmux to manage the windows/panes.
   Some useful tmux keyboard shortcuts are:
@@ -25,4 +18,12 @@ echo '
   Ctrl + b then ?           | Show shortcuts
 '
 read -p 'Press `enter` to continue'
-tmux attach-session -d
+if [[ $(tmux list-session -f "$APPNAME" 2> /dev/null) ]]; then
+    tmux new-session -A -s "$APPNAME"
+else
+    tmux new-session -AD -d -s "$APPNAME"
+    tmux send-keys -t 1 "tail -n 500 -F /opt/$APPNAME/logs/$APPNAME.log" Enter
+    #tmux selectp -t 1 -d #disable user input in pane
+    #tmux selectp -t 1 -e #enable user input in pane
+    tmux new-session -A -s "$APPNAME"
+fi
